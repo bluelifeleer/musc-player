50a7c81434df601f5a6eb456d4a1d57d2a616677
bluelife <thebulelife@163.com>
2018-07-12 11:03:37 +0800

    Initialized musc-player project

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import vueResource from 'vue-resource'
+import Store from './store'
+Vue.use(vueResource)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  Store,
+  components: { App },
+  template: '<App/>'
+})
